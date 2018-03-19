49fa31a284e40fd5adf47acd9c372c6e5de57c2d
Shubham Verma <lifemeansmore008@gmail.com>
2018-03-20 03:35:33 +0530

    Added Splash Screen

--- app/src/main/java/com/example/shubham/tourderaj/MainActivity.java
+++ app/src/main/java/com/example/shubham/tourderaj/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.shubham.tourderaj;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getBaseContext(),SecondActivity.class);
+                startActivity(intent);
+            }
+        },1500);
     }
 }
